8774b44b5ff2f9abcf32542de9bfe4a059d34594
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-01-30 14:39:03 +0100

    Oppskrift på korleis du kan forandre CSV-formatet som vert eksportert/lasta opp
    I fila ...\Classes\CSVFormat.cs kan du oppdatere format strengen som er
    definert i linje 43. Her representerar ein hakeparantes med eitt tal ein
    plassholdar for ein variabel {0}.
    
    Rekkjefølgja for variablane vert avgjort av rekkjefølgja dei er opplista
    i på linjene 46 og vidare.
    
    Konfigurasjonsfila som skal lastast frå serveren skal sjå ut slik som
    den du får når du lastar denne URLen:
    http://projects.europetech.eu/adaptive/getAdaptiveConfiguration.php

--- Avinet.Adaptive.Statistics.ExcelAddIn/Classes/CSVFormat.cs
+++ Avinet.Adaptive.Statistics.ExcelAddIn/Classes/CSVFormat.cs
@@ -40,7 +40,7 @@ namespace Avinet.Adaptive.Statistics.ExcelAddIn
 
         public string Serialize()
         {
-            var mStringFormat = "\"{0}\",\"{1}\",\"{2}\",\"{3}\",\"{4}\",\"{5}\",\"{6}\",\"{7}\",\"{8}\",\"{9}\",\"{10}\",\"{11}\",\"{12}\",\"{13}\",\"{14}\"";
+            var mStringFormat = "\"{0}\";\"{1}\";\"{2}\";\"{3}\";\"{4}\";\"{5}\";\"{6}\";\"{7}\";\"{8}\";\"{9}\";\"{10}\";\"{11}\";\"{12}\";\"{13}\";{14}";
 
             return String.Format(mStringFormat,
                 this.variable1,
@@ -52,12 +52,13 @@ namespace Avinet.Adaptive.Statistics.ExcelAddIn
                 this.krets_name,
                 this.kretstype_id,
                 this.region,
-                this.month,
                 this.day,
+                this.month,
                 this.quarter,
                 this.year,
-                this.value,
-                this.unit);
+                this.unit,
+                this.value
+                );
         }
 
     }
