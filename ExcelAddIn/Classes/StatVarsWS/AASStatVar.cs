25f5776e669b247dc7a722d9ab13fab259913518
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-05-12 02:25:04 +0200

    Statistics variable class

---
+++ ExcelAddIn/Classes/StatVarsWS/AASStatVar.cs
@@ -0,0 +1,25 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Avinet.Adaptive.Statistics.ExcelAddIn
+{
+    [Serializable()]
+    [XmlRoot(ElementName = "variableset", IsNullable=true, Namespace="http://tempuri.org/")]
+    [XmlType("variableset")]
+    public class AASStatVar
+    {
+        [XmlElement(ElementName="var1", IsNullable=true)]
+        public string var1 { get; set; }
+        [XmlElement(ElementName = "var2", IsNullable = true)]
+        public string var2 { get; set; }
+        [XmlElement(ElementName = "var3", IsNullable = true)]
+        public string var3 { get; set; }
+        [XmlElement(ElementName = "var4", IsNullable = true)]
+        public string var4 { get; set; }
+        [XmlElement(ElementName = "var5", IsNullable = true)]
+        public string var5 { get; set; }
+    }
+}
