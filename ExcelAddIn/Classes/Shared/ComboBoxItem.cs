1df4949d0a611d57694fbc53b182cef40bde238f
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-05-12 02:23:11 +0200

    New class to be used for binding items to combo-boxes

---
+++ ExcelAddIn/Classes/Shared/ComboBoxItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Avinet.Adaptive.Statistics.ExcelAddIn
+{
+    public class ComboBoxItem : IComparable
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+
+        public ComboBoxItem(string pKey, string pValue = null)
+        {
+            this.key = pKey;
+            this.value = pValue != null ? pValue : pKey;
+        }
+
+        public static ComboBoxItem GetNewItem(string pItem)
+        {
+            return new ComboBoxItem(pItem, pItem);
+        }
+
+        public static List<ComboBoxItem> MakeUniqueList(List<ComboBoxItem> pComboBoxItems)
+        {
+            return pComboBoxItems.Distinct(new ComboBoxItemComparer()).ToList<ComboBoxItem>();
+        }
+
+        // Implement IComparable CompareTo method - provide default sort order.
+        int IComparable.CompareTo(object pComboBoxItem)
+        {
+            var mComboBoxItem = (ComboBoxItem)pComboBoxItem;
+            return String.Compare(this.key, mComboBoxItem.key);
+        }
+
+    }
+
+    public class ComboBoxItemComparer : IEqualityComparer<ComboBoxItem>
+    {
+        public bool Equals(ComboBoxItem x, ComboBoxItem y)
+        {
+            return x.key == y.key;
+        }
+
+        public int GetHashCode(ComboBoxItem obj)
+        {
+            return obj.key.GetHashCode();
+        }
+    }
+}
