ec288452235905b03e8aff7942b7d9f8d64d6ea7
(Stein) Runar Bergheim <runarbe@gmail.com>
2014-05-12 02:24:02 +0200

    Enabled storage and restoration of import settings

---
+++ ExcelAddIn/Classes/StatePersistence/UploadFormFieldState.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Avinet.Adaptive.Statistics.ExcelAddIn
+{
+    /// <summary>
+    /// A class to hold the current state of a field
+    /// </summary>
+    [Serializable]
+    public class UploadFormFieldState
+    {
+        public string ExcelFieldName { get; set; }
+        public string StatVarLevel1 { get; set; }
+        public string StatVarLevel2 { get; set; }
+        public string StatVarLevel3 { get; set; }
+        public string StatVarLevel4 { get; set; }
+        public string StatVarLevel5 { get; set; }
+        public string MeasurementUnit { get; set; }
+        public string Year { get; set; }
+        public string Quarter { get; set; }
+        public string Month { get; set; }
+        public string Day { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public UploadFormFieldState()
+        {
+        }
+
+        public static List<UploadFormFieldState> GetFieldStates(DataGridView pDgv)
+        {
+            var mFields = new List<UploadFormFieldState>();
+            foreach (DataGridViewRow m in pDgv.Rows)
+            {
+                var mField = new UploadFormFieldState();
+                mField.ExcelFieldName = (string)m.Cells["Title"].Value;
+                mField.StatVarLevel1 = (string)m.Cells["StatVarCol1"].Value;
+                mField.StatVarLevel2 = (string)m.Cells["StatVarCol2"].Value;
+                mField.StatVarLevel3 = (string)m.Cells["StatVarCol3"].Value;
+                mField.StatVarLevel4 = (string)m.Cells["StatVarCol4"].Value;
+                mField.StatVarLevel5 = (string)m.Cells["StatVarCol5"].Value;
+                mField.MeasurementUnit = (string)m.Cells["MeasurementUnit"].Value;
+                mField.Year = (string)m.Cells["Year"].Value;
+                mField.Quarter = (string)m.Cells["Quarter"].Value;
+                mField.Month = (string)m.Cells["Month"].Value;
+                mField.Day = (string)m.Cells["Day"].Value;
+
+                mFields.Add(mField);
+            }
+            return mFields;
+        }
+
+    }
+}
---
+++ ExcelAddIn/Classes/StatePersistence/UploadFormStates.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace Avinet.Adaptive.Statistics.ExcelAddIn
+{
+    [Serializable]
+    public class UploadFormStates
+    {
+        public static JavaScriptSerializer Json = new JavaScriptSerializer(new SimpleTypeResolver());
+        public static string StateFile = "SavedImportSettings.json";
+        public List<UploadFormState> States { get; set; }
+        
+        public UploadFormStates()
+        {
+            this.States = new List<UploadFormState>();
+        }
+
+        public static UploadFormStates Load()
+        {
+            var mFilename = Util.GetFullResourceFilename(UploadFormStates.StateFile);
+            if (File.Exists(mFilename))
+            {
+                var mSettings = File.ReadAllText(mFilename);
+                if (mSettings != "")
+                {
+                    var mUploadFormStates = UploadFormStates.Json.Deserialize<UploadFormStates>(mSettings);
+                    if (mUploadFormStates != null)
+                    {
+                        return mUploadFormStates;
+                    }
+                }
+            }
+
+            var mEmptyUploadFormStates = new UploadFormStates();
+            UploadFormStates.Save(mEmptyUploadFormStates);
+            return mEmptyUploadFormStates;
+
+        }
+
+        public static UploadFormState GetState(string pStateId) {
+            var mSavedSettings = UploadFormStates.Load();
+            foreach (var mState in mSavedSettings.States)
+            {
+                if (mState.StateId == pStateId)
+                {
+                    return mState;
+                }
+            }
+            return null;
+        }
+
+        public static List<ComboBoxItem> GetComboBoxItems()
+        {
+            var mItems = new List<ComboBoxItem>();
+            var mSavedSettings = UploadFormStates.Load();
+            foreach (var mState in mSavedSettings.States)
+            {
+                mItems.Add(new ComboBoxItem(mState.StateName, mState.StateId));
+            }
+            return mItems;
+        }
+
+        public UploadFormStates AddState(String pName, UploadFormState pState)
+        {
+            pState.StateName = pName;
+            var mCurrentStates = UploadFormStates.Load();
+            var mToBeRemoved = new List<UploadFormState>();
+
+            // Identify elements to be removed
+            foreach (UploadFormState mState in mCurrentStates.States)
+            {
+                if (mState.StateName == pName)
+                {
+                    mToBeRemoved.Add(mState);
+                }
+            }
+
+            // Remove elements
+            foreach (var mState in mToBeRemoved)
+            {
+                mCurrentStates.States.Remove(mState);
+            }
+
+            // Add new element in its place
+            mCurrentStates.States.Add(pState);
+
+            // Save changes
+            UploadFormStates.Save(mCurrentStates);
+            return mCurrentStates;
+        }
+
+        public static UploadFormStates RemoveState(string pStateId)
+        {
+
+            var mCurrentStates = UploadFormStates.Load();
+            var mToBeRemoved = new List<UploadFormState>();
+
+            // Identify elements to be removed
+            foreach (UploadFormState mState in mCurrentStates.States)
+            {
+                if (mState.StateId == pStateId)
+                {
+                    mToBeRemoved.Add(mState);
+                }
+            }
+
+            // Remove elements
+            foreach (var mState in mToBeRemoved)
+            {
+                mCurrentStates.States.Remove(mState);
+            }
+
+            // Save changes
+            UploadFormStates.Save(mCurrentStates);
+            return mCurrentStates;
+        }
+
+        public static bool Save(UploadFormStates pUploadFormStates)
+        {
+            Util.SaveResourceFile(UploadFormStates.StateFile, UploadFormStates.Json.Serialize(pUploadFormStates));
+            return true;
+        }
+
+    }
+}
